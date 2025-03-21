c0c9cc2b8c095926f4b263a18e1d7c1dcaa85281
Murangiri772 <wanajesna@gmail.com>
2025-03-21 11:15:52 +0300

    initial commit

--- trileapp/urls.py
+++ trileapp/urls.py
@@ -7,10 +7,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('starter/', views.starter, name='starter'),
-    path('service/', views.service, name='service'),
-    path('portfolio/', views.portfolio, name='portfolio'),
+
     path('portfolio/', views.portfolio, name='portfolio'),
     path('services/', views.services, name='services'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+
+
+
+
 ]
--- trileapp/views.py
+++ trileapp/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+from trileapp.models import *
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 def starter(request):
     return render(request,'starter-page.html')
-def service(request):
-    return render(request, 'service-details.html')
-def portfolio(request):
-    return render(request, 'portfolio-details.html')
+
 def portfolio(request):
     return render(request, 'portfolio.html')
 def services(request):
@@ -17,4 +18,18 @@ def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
+def contact(request):
+    if request.method == "POST":
+        mycontact= Contact(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            subject=request.POST['subject'],
+            message=request.POST['message'],
+        )
+
+
+        mycontact.save()
+        return redirect('/contact')
+    else:
+        return render(request, 'contact.html')
 
